4f525716105be629c01a08010f332214f64f3f09
Valdenir Trevisan Junior <valdenirtrevi@gmail.com>
2021-05-28 19:40:29 -0300

    Commit inicial

---
+++ src/main/java/com/trevisan/poc/crud/PocCrudApplication.java
@@ -0,0 +1,13 @@
+package com.trevisan.poc.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevisan/poc/crud/PocCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevisan.poc.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
